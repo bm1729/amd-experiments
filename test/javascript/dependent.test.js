e84fad41e375531c4edcfd92a09562467def6a81
bm1729 <ben.mumford1729@gmail.com>
2015-07-20 12:55:45 +0000

    Added test without stub

--- test/javascript/dependent.test.js
+++ test/javascript/dependent.test.js
@@ -1,10 +1,14 @@
 define(['src/javascript/dependent', 'src/javascript/independent'], function(dependent, independent) {
     describe('dependent', function() {
 
-        it('getMessage', function() {
+        it('getMessage with stub', function() {
             sinon.stub(independent, 'getSubject').returns('Matt');
             expect(dependent.getMessage()).toEqual('Hello Matt!');
             independent.getSubject.restore();
+        });
+
+        it('getMessage without stub', function() {
+            expect(dependent.getMessage()).toEqual('Hello Ben!');
         });
 
     });
