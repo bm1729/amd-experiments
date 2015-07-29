5a8b16a7dc86f8a26580dc3e4b1cbd1609b19c7e
bm1729 <ben.mumford1729@gmail.com>
2015-07-29 09:24:35 +0000

    Updated squire test to use sinon mocks

--- test/javascript/dependent.squire.test.js
+++ test/javascript/dependent.squire.test.js
@@ -16,8 +16,14 @@ define(['node_modules/squirejs/src/Squire'], function(Squire) {
                 .mock('src/javascript/independent', {
                     getSubject: function() { return 'Dave'; }
                 })
-                .require(['src/javascript/dependent'], function(dependent) {
+                .store('src/javascript/independent')
+                .require(['src/javascript/dependent', 'mocks'], function(dependent, mocks) {
+                    var originalGetSubject = mocks.store['src/javascript/independent'].getSubject;
+                    var spy = sinon.stub(mocks.store['src/javascript/independent'], 'getSubject', originalGetSubject);
+                    
                     expect(dependent.getMessage()).toEqual('Hello Dave!');
+                    expect(spy.calledOnce).toBe(true);
+                    
                     done();
                 });
         });
